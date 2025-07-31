b178df3e0d86c7f2c884fd499812d60448f69697
qPels01 <petrlensin011@gmail.com>
2025-08-01 00:07:43 +0300

    мусор

--- backend/src/utils/verifyCodeGen.js
+++
@@ -1,9 +0,0 @@
-import bcrypt from "bcrypt";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-export function genCode() {
-    const code = Math.floor(10000 + Math.random() * 90000).toString();
-    return bcrypt.hash(code, process.env.SALT_ROUND);
-}
